ada7262d07108f5d1dd1b39d9963e21814d26629
Shujaat Bakhsh <shujaatbakhsh25@gmail.com>
2019-11-09 17:12:49 -0500

    First push

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 import App from './components/App';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
